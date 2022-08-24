f4deccd1d9e812eae560004f387d30980cce96f2
Nugie Jaya Nugraha <Nugie Jaya Nugraha>
2022-08-24 19:22:01 +0700

    edit app listen

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 4000;
+// const PORT = process.env.PORT || 4000;
 const cors = require("cors");
 
 // import router
@@ -15,6 +15,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/", router);
 
 // run app in port 4000
-app.listen(PORT, () => {
-  console.log(`Listening on http://localhost:${PORT}`);
+app.listen(process.env.PORT || 4000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
